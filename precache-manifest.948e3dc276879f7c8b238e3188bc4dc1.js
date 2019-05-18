13abeda65e11c5d09d3eae490795540bbc0cd633
Amit Dhawan <amit.dhawan@monotype.com>
2019-05-18 17:09:58 +0530

    Updates

---
+++ precache-manifest.948e3dc276879f7c8b238e3188bc4dc1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e3c267abefc2c880506e67d8d10458c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c68dd40f88b5c8ba2379",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "c68dd40f88b5c8ba2379",
+    "url": "/static/js/main.7fb8e1ff.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
