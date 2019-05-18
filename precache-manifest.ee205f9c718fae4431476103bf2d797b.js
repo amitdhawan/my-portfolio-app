fbe019fbd93acc6e1b389023821f509c0ee8af05
Amit Dhawan <amit.dhawan@monotype.com>
2019-05-18 19:07:04 +0530

    Updates

---
+++ precache-manifest.ee205f9c718fae4431476103bf2d797b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0e918cda12f55f1bd06fa436cc0a61b",
+    "url": "/my-portfolio-app/index.html"
+  },
+  {
+    "revision": "5d10d6803f7f23ad87fe",
+    "url": "/my-portfolio-app/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/my-portfolio-app/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "5d10d6803f7f23ad87fe",
+    "url": "/my-portfolio-app/static/js/main.a52fa7e6.chunk.js"
+  },
+  {
+    "revision": "cbbc5dbd6cd384a24549",
+    "url": "/my-portfolio-app/static/js/runtime~main.32d670a5.js"
+  }
+]);
